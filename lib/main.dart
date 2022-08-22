daa6eea3698701b52c1cc81206828f8e7626730f
NazirHakim <nazirhakim148@gmail.com>
2022-08-23 01:47:09 +0400

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/home_page.dart';
 import 'package:matcher/matcher.dart';
 
- void main(){
+void main() {
   runApp(Myapp());
- }
+}
 
 class Myapp extends StatelessWidget {
   const Myapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-            ),
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
